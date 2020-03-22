a4208188683072db07744032c0a64178fd0ba3b6
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2020-03-22 15:09:09 +0900

    20200322_3

---
+++ Day01/00.예제/01_python200_src/033.py
@@ -0,0 +1,8 @@
+strdata1 = '나는 파이썬 프로그래머다' 	# 문자열 선언 방법 1
+strdata2 = "You are a programmer" 	# 문자열 선언 방법 2
+strdata3 = """I love 		# 문자열 선언 방법 3
+python. You love
+python too!
+"""
+strdata4 = "My son's name is John" 	# " " 안에서 ' 사용하기
+strdata5 = '문자열 "abc"의 길이는 3입니다.'    # ' ' 안에서 " 사용하기
